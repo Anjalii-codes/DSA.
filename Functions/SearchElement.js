a1249dac1a62488ad030feb4f3454ab2aca06ca6
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-01 14:55:01 +0530

    Search Element

---
+++ Functions/SearchElement.js
@@ -0,0 +1,11 @@
+function SearchElement(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == element) {
+            return i
+        }
+    }
+    return -1
+}
+
+let arr = [10, 20, 31, 40, 50, 60, 7, 80, 90, 100];
+console.log(SearchElement(arr, 80))
