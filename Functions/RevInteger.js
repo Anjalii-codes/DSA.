cdeb44ea0bf755c152e02af3c8f553e907a0c1f1
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-02 15:01:46 +0530

    Reverse Integer

---
+++ Functions/RevInteger.js
@@ -0,0 +1,21 @@
+function ReverseInteger(n) {
+    ncopy = n
+    n = Math.abs(n)
+    
+    let rev = 0
+    while (n > 0) {
+        let rem = n % 10
+        rev = (10 * rev) + rem
+        n = Math.floor(n / 10)
+        
+    }
+    let limit = 2 ** 31
+    if (rev > limit || rev < -limit) {
+        return 0
+    }
+    return ncopy<0? -rev : rev
+    
+}
+let num = -956;
+let res = ReverseInteger(num);
+console.log(res);
