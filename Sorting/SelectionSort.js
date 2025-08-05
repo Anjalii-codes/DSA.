b525fd5350323876675c52601df78a3c6b4ff314
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-05 18:07:58 +0530

    Selection Sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,17 @@
+function SelectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+
+let arr = [96, 58, 64, 96, 98, 74, 700, 96];
+console.log(SelectionSort(arr));
