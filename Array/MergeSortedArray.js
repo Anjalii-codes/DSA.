67743117c918a30d6f572f999cb719c11542f6d9
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-06 16:37:31 +0530

    Merge Sorted Array

---
+++ Array/MergeSortedArray.js
@@ -0,0 +1,29 @@
+
+
+function merge(nums1, m, nums2, n) {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    while (i < m && j < n) {
+        if (nums1[i] < nums2[j]) {
+            nums1[k++] = nums1[i++];
+        } else {
+            nums1[k++] = nums2[j++];
+        }
+    }
+    while (i < m) {
+        nums1[k++] = nums1[i++];
+    }
+    while (j < n) {
+        nums1[k++] = nums2[j++];
+    }
+    return nums1;
+}
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+let res = merge(nums1, m, nums2, n);
+console.log(res);
