c1ca56711793907106e1edae60a209f90d25d3bf
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-07 17:13:37 +0530

    chocolate distribution

---
+++ Array/chocolateDistribution.js
@@ -0,0 +1,14 @@
+let arr = [3, 4, 1, 9, 5, 6, 7, 9,  2];
+arr.sort();
+let i = 0;
+let m=5
+let diff= Infinity
+j = i + (m - 1);
+while (j < arr.length) {
+    if (arr[j] - arr[i] < diff) {
+        diff = arr[j] - arr[i];
+    }
+    i++;
+    j++;
+}
+console.log(diff)
