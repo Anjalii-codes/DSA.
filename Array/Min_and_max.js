4b8b11475aea073ba4d6008e6cf833ffff92d9ff
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-08-07 12:24:20 +0530

    Min Max in minimum comparisions

---
+++ Array/Min_and_max.js
@@ -0,0 +1,48 @@
+let arr = [2, 0, 9, 3, 5,  1];
+let min;
+let max;
+let i;
+let n = arr.length;
+if (n % 2 == 0) {
+    if(arr[0] > arr[1]){
+        min = arr[1]
+        max = arr[0]
+    }
+    else {
+        min = arr[0]
+        max = arr[1]
+
+    }
+    i = 2
+
+}
+else {
+    min = arr[0]
+    max = arr[0]
+    i = 1
+}
+while (i < n - 1) {
+    if (arr[i] < arr[i + 1]) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i + 1] > max) {
+            max = arr[i + 1];
+        }
+    }
+    else {
+        if (arr[i + 1] < min) {
+            min = arr[i + 1];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+            
+            
+    }
+    i+=2
+
+
+    
+}
+console.log(min, max)
